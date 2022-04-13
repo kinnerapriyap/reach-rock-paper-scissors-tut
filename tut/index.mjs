2a5d0e0870cf96192205948298934026668400ac
Kinnera Putti <kinnera.putti@ioki.com>
2022-04-13 20:53:05 +0200

    Add for Rock, Paper and Scissors

--- tut/index.mjs
+++ tut/index.mjs
@@ -9,11 +9,24 @@ const accB = await stdlib.newTestAccount(startingBalance);
 const ctcA = accA.contract(backend);
 const ctcB = accB.contract(backend, ctcA.getInfo());
 
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['B wins', 'Draw', 'A wins'];
+const Player = (Who) => ({
+  getHand: () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    return hand;
+  },
+  seeOutcome: (outcome) => {
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+  },
+});
+
 await Promise.all([
-    ctcA.p.A({
-    
+  ctcA.p.A({
+    ...Player('A'),
   }),
   ctcB.p.B({
-    
+    ...Player('B'),
   }),
 ]);
